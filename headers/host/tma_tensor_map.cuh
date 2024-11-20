6e4a82aa45bcb86c592d2817575fb287ac23ef3f
Yicong Eddy Luo <m1662576219@gmail.com>
2024-11-19 19:00:39 -0500

    add_swizzle

--- headers/host/tma_tensor_map.cuh
+++ headers/host/tma_tensor_map.cuh
@@ -44,7 +44,7 @@ CUtensorMap create_1d_tensor_map(uint64_t tensor_dim, uint32_t tile_dim, void *t
       box_size,    // const cuuint32_t *boxDim,
       elem_stride, // const cuuint32_t *elementStrides,
       CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_NONE,
-      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_32B,
+      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_NONE,
       CUtensorMapL2promotion::CU_TENSOR_MAP_L2_PROMOTION_NONE,
       CUtensorMapFloatOOBfill::CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
 
@@ -89,7 +89,7 @@ CUtensorMap create_2d_tensor_map(uint64_t tensor_dim1, uint64_t tensor_dim2, uin
       elem_stride, // const cuuint32_t *elementStrides,
       CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_NONE,
       CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_NONE,
-      CUtensorMapL2promotion::CU_TENSOR_MAP_L2_PROMOTION_NONE,
+      CUtensorMapL2promotion::CU_TENSOR_MAP_SWIZZLE_32B,
       CUtensorMapFloatOOBfill::CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
 
   assert(res == CUDA_SUCCESS && "tensormap creation failed.");
