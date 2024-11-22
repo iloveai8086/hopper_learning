e5fc5feb0d8c01eb353f645d0053860c442e2100
Yicong Eddy Luo <m1662576219@gmail.com>
2024-11-22 18:03:02 -0500

    update

--- headers/host/tma_tensor_map.cuh
+++ headers/host/tma_tensor_map.cuh
@@ -128,8 +128,8 @@ CUtensorMap create_2d_tensor_map_half(uint64_t tensor_dim1, uint64_t tensor_dim2
       stride,      // const cuuint64_t *globalStrides,
       box_size,    // const cuuint32_t *boxDim,
       elem_stride, // const cuuint32_t *elementStrides,
-      CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_NONE,
-      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_16B,
+      CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_32B,
+      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_32B,
       CUtensorMapL2promotion::CU_TENSOR_MAP_L2_PROMOTION_NONE,
       CUtensorMapFloatOOBfill::CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
 
