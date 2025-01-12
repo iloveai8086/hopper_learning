56309b3ff69e5b2e1a8d28faa14c4cee1daa1cb2
Yicong Eddy Luo <m1662576219@gmail.com>
2025-01-12 14:55:38 -0500

    update

--- headers/device/wgmma.cuh
+++ headers/device/wgmma.cuh
@@ -105,3 +105,21 @@ __device__ GmmaDescriptor make_desc(PointerType smem_ptr)
   desc.bitfield.base_offset_ = 0;
   return desc;
 }
+
+__device__ void MMA_SP_WRAPPER(uint32_t * c, GmmaDescriptor desc_a, GmmaDescriptor desc_b, uint32_t metadata) {
+	asm volatile("wgmma.mma_sync.sp.aligned.m64n8k32.f16.f16.f16 "
+				 "{%0, %1}, " // c
+				 "%2, %3, "	  // desc A, B
+				 "%4, "		  // meta
+				 "0, "		  // thread selection
+				 "1, "		  // scale D
+				 "%7, %8, "	  // +/- scale A, B
+				 "%9, %10;"	  // transpose A, B
+				 : "+r"(c[0]), "+r"(c[1])
+				 : "l"(desc_a), "l"(desc_b),
+				   "r"(metadata),	// metadata
+				   "r"(0),			// thread selection
+				   "r"(1),			// scale D
+				   "n"(1), "n"(1),	// +- scale A, B
+				   "n"(0), "n"(1)); // transpose A, B
+}
